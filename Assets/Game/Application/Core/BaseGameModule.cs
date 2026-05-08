754cfebaf0c91b662601f0cbdb15817aa4920bc1
ADMIN\Admin <emokiller22101993@gmail.com>
2026-05-08 16:58:36 +0700

    no message

--- Assets/Game/Application/Core/BaseGameModule.cs
+++ Assets/Game/Application/Core/BaseGameModule.cs
@@ -20,26 +20,17 @@ namespace Game.Application.Core
                 return _cachedModuleName;
             }
         }
-
-        public bool IsInitialized { get; protected set; }
-
         public abstract Type[] GetDependencies();
 
         public async UniTask InitializeAsync(IServiceContainer services, CancellationToken ct = default)
         {
-            if (IsInitialized) 
-                return;
-
             try 
             {
                 ct.ThrowIfCancellationRequested();
                 await OnInitializeAsync(services, ct);
-                ct.ThrowIfCancellationRequested();
-                IsInitialized = true;
             }
             catch (OperationCanceledException)
             {
-                // Log dạng Warning hoặc im lặng vì đây là hành vi bình thường
                 Debug.LogWarning($"[Module] {ModuleName} initialization was canceled.");
                 throw; // Ném ngược ra để phía gọi (Caller) biết là task đã dừng
             }
@@ -50,7 +41,6 @@ namespace Game.Application.Core
             }
         }
 
-        // Các module con bắt buộc phải thực thi hàm này
         protected abstract UniTask OnInitializeAsync(IServiceContainer services, CancellationToken ct);
 
         public abstract void Shutdown();
